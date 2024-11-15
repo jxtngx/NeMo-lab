a2856d1f2bd4aa4499e0f61595e53ab751c390df
Justin Goheen <26209687+jxtngx@users.noreply.github.com>
2024-11-15 17:33:59 -0500

    add HFLlamaImporter from NeMo

--- src/nemo_lab/model/llama32.py
+++ src/nemo_lab/model/llama32.py
@@ -19,74 +19,47 @@ from nemo.collections.llm.gpt.model.llama import Llama3Config
 
 
 @dataclass
-class Llama32Config1B(Llama3Config):
-    attention_bias = False
-    attention_dropout = 0.0
-    bos_token_id = 128000
-    eos_token_id = 128001
-    head_dim = 64
-    hidden_act = "silu"
-    hidden_size = 2048
-    initializer_range = 0.02
-    intermediate_size = 8192
-    max_position_embeddings = 131072
-    mlp_bias = False
-    model_type = "llama"
-    num_attention_heads = 32
-    num_hidden_layers = 16
-    num_key_value_heads = 8
-    pretraining_tp = 1
-    rms_norm_eps = 1e-05
-    rope_scaling = {
-        "factor": 32.0,
-        "high_freq_factor": 4.0,
-        "low_freq_factor": 1.0,
-        "original_max_position_embeddings": 8192,
-        "rope_type": "llama3",
-    }
-    rope_theta = 500000.0
-    tie_word_embeddings = True
-    torch_dtype = "bfloat16"
-    transformers_version = "4.45.0.dev0"
-    use_cache = True
-    vocab_size = 128256
+class Llama32Config(Llama3Config):
+    init_method_std = 0.02
+    ffn_hidden_size = 8192
+    layernorm_epsilon = 1e-05
+    num_query_groups = 8
+    rotary_base = 500000.0
+    scale_factor: int = 32
+    low_freq_factor: int = 1
+    high_freq_factor: int = 4
+    make_vocab_size_divisible_by = 128
+
+    # #### conversion mapping from HFLlamaImporter #### #
+    # num_layers=source.num_hidden_layers
+    # hidden_size=source.hidden_size
+    # ffn_hidden_size=source.intermediate_size
+    # num_attention_heads=source.num_attention_heads
+    # init_method_std=source.initializer_range
+    # layernorm_epsilon=source.rms_norm_eps
+    # num_query_groups=source.num_key_value_heads
+    # rotary_base=source.rope_theta
 
 
 @dataclass
-class Llama32Config3B(Llama3Config):
-    attention_bias = False
-    attention_dropout = 0.0
-    bos_token_id = 128000
-    eos_token_id = 128001
-    head_dim = 128
-    hidden_act = "silu"
+class Llama32Config1B(Llama32Config):
+    hidden_size = 2048
+    num_attention_heads = 32
+    num_layers = 16
+
+
+@dataclass
+class Llama32Config3B(Llama32Config):
     hidden_size = 3072
-    initializer_range = 0.02
-    intermediate_size = 8192
-    max_position_embeddings = 131072
-    mlp_bias = False
-    model_type = "llama"
     num_attention_heads = 24
-    num_hidden_layers = 28
-    num_key_value_heads = 8
-    pretraining_tp = 1
-    rms_norm_eps = 1e-05
-    rope_scaling = {
-        "factor": 32.0,
-        "high_freq_factor": 4.0,
-        "low_freq_factor": 1.0,
-        "original_max_position_embeddings": 8192,
-        "rope_type": "llama3",
-    }
-    rope_theta = 500000.0
-    tie_word_embeddings = True
-    torch_dtype = "bfloat16"
-    transformers_version = "4.45.0.dev0"
-    use_cache = True
-    vocab_size = 128256
+    num_layers = 28
 
 
-class Llama32Model1B(llm.LlamaModel): ...
+class Llama32Model1B(llm.LlamaModel):
+    def __init__(self, config=Llama32Config1B, optim=None, tokenizer=None, model_transform=None):
+        super().__init__(config, optim, tokenizer, model_transform)
 
 
-class Llama32Model3B(llm.LlamaModel): ...
+class Llama32Model3B(llm.LlamaModel):
+    def __init__(self, config=Llama32Config3B, optim=None, tokenizer=None, model_transform=None):
+        super().__init__(config, optim, tokenizer, model_transform)
