3cc8574bd47c1ca785c531aec22a8a50cdc6d534
Justin Goheen <26209687+jxtngx@users.noreply.github.com>
2024-11-15 15:00:37 -0500

    remove unused code

--- src/nemo_lab/model/llama32.py
+++ src/nemo_lab/model/llama32.py
@@ -12,13 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import os
 from dataclasses import dataclass
 
 from nemo.collections import llm
 from nemo.collections.llm.gpt.model.llama import Llama3Config
-
-ckpts = os.path.join(".models/pretained")
 
 
 @dataclass
