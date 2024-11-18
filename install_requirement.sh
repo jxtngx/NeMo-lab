7121b197943fbe4a03dcae60e88f4f09b2d262c6
jxtngx <26209687+jxtngx@users.noreply.github.com>
2024-11-17 20:26:40 -0500

    add nvidia/apex

--- install_requirement.sh
+++ install_requirement.sh
@@ -1,3 +1,4 @@
 pip install -e ".[all]"
 pip install git+https://github.com/NVIDIA/NeMo-Run.git
 pip install git+https://github.com/NVIDIA/Megatron-LM.git
+pip install -v --disable-pip-version-check --no-cache-dir --no-build-isolation --config-settings "--build-option=--cpp_ext" --config-settings "--build-option=--cuda_ext" git+https://github.com/NVIDIA/apex.git
