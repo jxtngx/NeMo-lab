3f67c0f5da34624f226a147b66d5bfb2b1416536
jxtngx <26209687+jxtngx@users.noreply.github.com>
2024-11-18 08:15:03 -0500

    add NeMo 2.0 finetune quickstart

---
+++ scripts/quickstart/nemo2_quickstart.py
@@ -0,0 +1,34 @@
+from pathlib import Path
+from pytorch_lightning.utilities.exceptions import MisconfigurationException
+
+from nemo.collections import llm
+import nemo_run as run
+
+# configure the model
+config = llm.Llama31Config8B()
+model = llm.LlamaModel(
+    config=config,
+)
+
+try:
+    llm.import_ckpt(
+        model=model,
+        source="hf://meta-llama/Llama-3.1-8B",
+        overwrite=True,
+    )
+except MisconfigurationException as e:
+    print(e)
+
+
+recipe = llm.llama31_8b.finetune_recipe(
+    name="llama31_8b_finetuning",
+    dir="finetune-logs",
+    num_nodes=1,
+    num_gpus_per_node=1,
+    peft_scheme="lora",  # 'lora', 'none'
+    packed_sequence=False,
+)
+
+recipe.trainer.strategy = "auto"  # let PTL do the work
+
+run.run(recipe, executor=run.LocalExecutor())
