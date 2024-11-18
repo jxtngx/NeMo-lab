688414dec4a92e90b8626c62c91394d24f3f5b47
jxtngx <26209687+jxtngx@users.noreply.github.com>
2024-11-18 08:22:13 -0500

    update requirements

---
+++ install_requirements.sh
@@ -0,0 +1,6 @@
+# apt-get update && apt-get install -y libsndfile1 ffmpeg
+pip install Cython packaging
+pip install nemo_toolkit['all']
+pip install git+https://github.com/NVIDIA/NeMo-Run.git
+pip install git+https://github.com/NVIDIA/Megatron-LM.git
+pip install -v --disable-pip-version-check --no-cache-dir --no-build-isolation --config-settings "--build-option=--cpp_ext" --config-settings "--build-option=--cuda_ext" git+https://github.com/NVIDIA/apex.git
