c4f80a6e40ade4d028f0e3ec559bfb77730b702b
Justin Goheen <26209687+jxtngx@users.noreply.github.com>
2024-11-22 09:21:36 -0500

    revert to apex python build

--- install_requirements.sh
+++ install_requirements.sh
@@ -3,8 +3,11 @@ pip install Cython packaging
 pip install nemo_toolkit['all']
 pip install git+https://github.com/NVIDIA/NeMo-Run.git
 pip install git+https://github.com/NVIDIA/Megatron-LM.git
-pip install -v \
-    --disable-pip-version-check \
-    --no-cache-dir --no-build-isolation --config-settings \
-    "--build-option=--cpp_ext" --config-settings "--build-option=--cuda_ext" \
-    git+https://github.com/NVIDIA/apex.git
+# apex python
+pip install -v --disable-pip-version-check --no-build-isolation --no-cache-dir git+https://github.com/NVIDIA/apex.git
+# apex c++ extensions
+# pip install -v \
+#     --disable-pip-version-check \
+#     --no-cache-dir --no-build-isolation --config-settings \
+#     "--build-option=--cpp_ext" --config-settings "--build-option=--cuda_ext" \
+#     git+https://github.com/NVIDIA/apex.git
